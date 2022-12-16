2c225be9ffa266f6188702d16c432aee51970b05
Bobby Reed <bobby.karma_xl@yahoo.com>
2022-12-16 13:14:59 -0600

    Add isValidTable function to tables.service

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,11 +1,45 @@
 const service = require("./tables.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const { as } = require("../db/connection");
 
-async function list(req, res, next) {
-    res.json({
-        data: await service.list()
-    })
+const VALID_FIELDS = [ "table_name", "capacity" ];
+
+function isValidTable(req, res, next) {
+    const table = req.body.data;
+    if (!table) {
+        return next({
+            status: 400,
+            message: "Must have data property"
+        });
+    }
+
+    VALID_FIELDS.forEach((field) => {
+        if (!table[field]) {
+            return next({
+                status: 400,
+                message: `Must have ${field} property.`
+            });
+        }
+    });
+
+    if (typeof table["capacity"] !== "number") {
+        return next({
+            status: 400,
+            message: "Capacity must be a number greater than 0",
+        });
+    }
+
+    next();
 }
+
+async function list(req, res, next) {}
+
+async function create(req, res, next) {}
+
+
 
 module.exports = {
-    list,
-}
+    list: asyncErrorBoundary(list),
+    create: [ isValidTable, asyncErrorBoundary(create) ],
+    isValidTable,
+};
