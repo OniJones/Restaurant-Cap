0ce3a6028c59ff83a367af803943fcd0fc33a66c
Bobby Reed <bobby.karma_xl@yahoo.com>
2022-12-16 11:10:49 -0600

    Add create function to tables.service

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -6,6 +6,14 @@ function list() {
         .orderBy("tables.table_name");
 }
 
+function create(table) {
+    return knex("tables")
+        .insert(table)
+        .returning("*")
+        .then((newTables) => newTables[0]);
+}
+
 module.exports = {
     list,
+    create,
 }
