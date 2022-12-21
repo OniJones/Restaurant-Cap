bbff9324bd6023023d1b825ac16d695293a175c5
Bobby Reed <bobby.karma_xl@yahoo.com>
2022-12-21 09:54:21 -0600

    Fixed TableList.js

--- front-end/src/dashboard/tableList/tableList.js
+++ front-end/src/dashboard/tableList/tableList.js
@@ -2,7 +2,15 @@ import React from "react";
 import TableInfo from "./TableInfo";
 
 export default function TableList({ tables, loadDashboard }) {
+if(!tables) {
+    return null;
+}
 
+const formatted = tables.map((table) => {
+    return (
+        <TableInfo key={table.table_id} table={table} loadDashboard={loadDashboard} />
+    );
+});
 
     return(
         <div>
